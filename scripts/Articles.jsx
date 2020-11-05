13dd51ae67e914172b9d44370ee4b986ccc9363f
Tim Russo <ec2-user@ip-172-31-73-67.ec2.internal>
2020-11-05 22:08:40 +0000

    Get articles/FAQs from server, display temp placeholders

--- scripts/Articles.jsx
+++ scripts/Articles.jsx
@@ -1,8 +1,28 @@
 import * as React from 'react';
+import { Socket } from './Socket';
 
 export function Articles() {
+  const [article, setArticle] = React.useState([]);
+
+  function getArticles() {
+    React.useEffect(() => {
+      Socket.on('article list', (data) => {
+        setArticle(data.articles);
+      });
+    });
+  }
+
+  getArticles();
 
   return (
-    <h1>COVID-19 Articles</h1>
+    <div id="article-div">
+      <h1 className="article-h1">COVID-19 Articles</h1>
+      <ul className="articles-ul">
+        <li>
+          <p>Article title</p>
+          <p>Link to article</p>
+        </li>
+      </ul>
+    </div>
   );
 }
--- scripts/Faq.jsx
+++ scripts/Faq.jsx
@@ -1,8 +1,28 @@
 import * as React from 'react';
+import { Socket } from './Socket';
 
 export function Faq() {
+  const [faq, setFaq] = React.useState([]);
+
+  function getFaqs() {
+    React.useEffect(() => {
+      Socket.on('faq list', (data) => {
+        setFaq(data.faqs);
+      });
+    });
+  }
+
+  getFaqs();
 
   return (
-    <h1>Frequently Asked Questions</h1>
+    <div id="faq-div">
+      <h1 className="faq-h1">Frequently Asked Questions</h1>
+      <ul className="faq-ul">
+        <li>
+          <p>Question</p>
+          <p>Answer</p>
+        </li>
+      </ul>
+    </div>
   );
 }
