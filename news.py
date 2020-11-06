54556b488529c5d120b4544dcb69547db1fb1081
EC2 Default User <ec2-user@ip-172-31-29-195.ec2.internal>
2020-11-06 02:24:40 +0000

    Add comments to help integration into back end

--- news.py
+++ news.py
@@ -4,12 +4,29 @@ from datetime import date
 from datetime import timedelta 
 
 YESTERDAY = date.today() - timedelta(days = 1) 
-SOURCES = 'abc-news,associated-press,bloomberg,cbs-news,cnn,fox-news,google-news,independent,msnbc,medical-news-today,nbc-news,the-hill,usa-today'
+SOURCES = ('abc-news,associated-press,bloomberg,'
+        'cbs-news,cnn,fox-news,google-news,'
+        'independent,msnbc,medical-news-today,'
+        'nbc-news,the-hill,usa-today')
 
 def get_news(amtArticles, since = YESTERDAY.strftime("%yyyy-%mm-%dd"), query = 'covid'):
+    '''
+    This function is used to pull articles from NEWS API
+        PARAMERTERS:
+            amtArticles: the amount of articles you would like to be returned
+            since: what is the oldest you would like an article to be
+                DEFAULT: yesterday's date
+            query: what one keyword would you liek to query for
+                DEFUALT: covid
+        RETURNS
+            an array of Article objects
+    '''    
     
     if amtArticles < 1: 
         return {'Error':'Amount of articles must be > 0'}
+    
+    #ensure that the query is one word
+    query = query.split()[0]
     
     url = ('http://newsapi.org/v2/top-headlines?'
             'sources='+SOURCES+'&'
@@ -54,3 +71,24 @@ class Article:
         self.image = image
         self.publishDate = publishDate
         self.url = url
+        
+'''
+{
+    "status": "ok",
+    "totalResults": 225,
+    -"articles": [
+        -{
+        -"source": {
+            "id": "axios",
+            "name": "Axios"
+        },
+        "author": "Jacob Knutson",
+        "title": "\"This is getting insane\": Republicans rebuke Trump over baseless election claims",
+        "description": "\"STOP Spreading debunked misinformation,\" Rep. Adam Kinzinger said.",
+        "url": "https://www.axios.com/trump-republicans-baseless-election-claims-fa686850-efb9-41cd-a25e-1b9cc1b52b85.html",
+        "urlToImage": "https://images.axios.com/OgCs7tNiHvLM8zUDGuXgCOlqgZM=/fit-in/1366x1366/2020/11/06/1604623553559.jpg",
+        "publishedAt": "2020-11-06T01:29:23Z",
+        "content": "A growing list of Republicans have reproached President Trump for his baseless claims of widespread voter fraud.\r\nWhy it matters: In televised remarks on Thursday evening. the president provided no e… [+1962 chars]"
+    },
+-{
+'''
