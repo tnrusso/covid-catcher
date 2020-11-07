34d833cfc8bb857e83205221dd36fd5f2274d1ca
Tim Russo <ec2-user@ip-172-31-73-67.ec2.internal>
2020-11-07 04:50:43 +0000

    Add files for routing and navigation bar

--- scripts/Main.jsx
+++ scripts/Main.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { Login } from './Login';
+import { Content } from './Content';
 
-ReactDOM.render(<Login />, document.getElementById('content'));
+ReactDOM.render(<Content />, document.getElementById('content'));
