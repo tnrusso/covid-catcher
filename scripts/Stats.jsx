9e771bc2a566f465bc4386492b643eafc2c5decf
andreapaz7 <ec2-user@ip-172-31-20-226.ec2.internal>
2020-11-22 06:29:41 +0000

    changed pie chart to bar graph

--- scripts/Stats.jsx
+++ scripts/Stats.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { PieChart } from 'react-minimal-pie-chart';
+import {
+  BarChart, Bar, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 import { Socket } from './Socket';
 
 export function Stats() {
@@ -12,54 +14,51 @@ export function Stats() {
 
   const chartData = [];
 
-  function randomColor() {
-    const num = Math.floor(Math.random() * 16777215).toString(16);
-    return `#${num.toString()}`;
-  }
-
   function getStats() {
     React.useEffect(() => {
       Socket.on('stats', (data) => {
         setCases(data.cases);
         setDeaths(data.deaths);
         setRecovered(data.recovered);
-        setState(data.state); // GET NAME OF STATE
-        setCountyNames(data.countyNames); // GET ARRAY OF COUNTY NAMES
-        setCountyStats(data.countyStats); // GET ARRAY OF # IN COUNTY
+        setState(data.state);
+        setCountyNames(data.countyNames);
+        setCountyStats(data.countyStats);
       });
       return () => Socket.off('stats');
     });
   }
 
   getStats();
-  countyNames.map((item, index) => (chartData.push({ title: `${item} ${countyStats[index]}`, value: countyStats[index], color: randomColor() })));
+  countyNames.map((item, index) => (chartData.push({ county: item, cases: countyStats[index] })));
 
   return (
     <div id="stats-div">
       <h1 className="stats-h1">Current Statistics</h1>
       <h2>{state}</h2>
       <h2>
-        Total cases:
-        {cases.toLocaleString()}
+        {`Total cases: ${cases.toLocaleString()}`}
       </h2>
       <h2>
-        Total number of deaths:
-        {deaths.toLocaleString()}
+        {`Total number of deaths: ${deaths.toLocaleString()}`}
       </h2>
       <h2>
-        Total number of recovered:
-        {recovered.toLocaleString()}
+        {`Total number of recovered: ${recovered.toLocaleString()}`}
       </h2>
 
-      <div id="pie-chart">
-        <PieChart
+      <div id="chart">
+        <BarChart
+          width={900}
+          height={400}
           data={chartData}
-          animate
-          label={({ dataEntry }) => dataEntry.title}
-          labelStyle={{ fontSize: '2' }}
-          labelPosition={90}
-          radius={45}
-        />
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="county" />
+          <YAxis />
+          <Tooltip />
+          <Legend verticalAlign="top" wrapperStyle={{ lineHeight: '40px' }} />
+          <ReferenceLine y={0} stroke="#000" />
+          <Bar dataKey="cases" fill="#82ca9d" />
+        </BarChart>
       </div>
     </div>
   );
