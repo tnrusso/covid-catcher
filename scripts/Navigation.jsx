346e27596a78d4d8621b2ec3f888306139575e63
Tim Russo <ec2-user@ip-172-31-73-67.ec2.internal>
2020-11-27 17:10:49 +0000

    Remove search bar from navigation bar

--- scripts/Navigation.jsx
+++ scripts/Navigation.jsx
@@ -1,16 +1,6 @@
 import * as React from 'react';
 
 export function Navigation() {
-  const [text, setText] = React.useState('');
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setText('');
-  }
-
-  function handleChange(e) {
-    setText(e.target.value);
-  }
 
   return (
     <nav className="navbar navbar-inverse">
@@ -26,10 +16,6 @@ export function Navigation() {
           <li><a href="/statistics">Statistics</a></li>
           <li><a href="/questionnaire">Questionnaire</a></li>
         </ul>
-        <form onSubmit={handleSubmit} className="navbar-form d-flex justify-content-center w-100 navbar-right">
-          <input type="text" value={text} onChange={handleChange} className="form-control" />
-          <button type="submit" className="btn btn-danger">Search</button>
-        </form>
       </div>
     </nav>
   );
