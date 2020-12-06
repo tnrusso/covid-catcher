5790a4ba4f320d1a087688574569e969631f36e8
Tim Russo <ec2-user@ip-172-31-73-67.ec2.internal>
2020-12-06 23:35:36 +0000

    Lint stats page

--- scripts/Stats.jsx
+++ scripts/Stats.jsx
@@ -4,11 +4,10 @@ import {
 } from 'recharts';
 import { Menu } from './Menu';
 import { Socket } from './Socket';
-import ReactDOM from 'react-dom';
 
 export function Stats() {
   const [cases, setCases] = useState([]);
-  const [newCases,setNewCases] = useState([]);
+  const [newCases, setNewCases] = useState([]);
   const [deaths, setDeaths] = useState([]);
   const [newDeaths, setNewDeaths] = useState([]);
   const [recovered, setRecovered] = useState([]);
@@ -16,29 +15,25 @@ export function Stats() {
   const [countyNames, setCountyNames] = useState([]);
   const [countyCases, setCountyCases] = useState([]);
   const [countyDeaths, setCountyDeaths] = useState([]);
-  const [s, setS] = useState('');
-  
-  const[chartKey, setChartKey]=useState('cases');
-  var chartData=getChartData();
-  var chartColor = '#2E607D';
-  
-  function getChartData(){
-    var data=[];
-    if(chartKey==='cases') {
+
+  const [chartKey, setChartKey] = useState('cases');
+  let chartData = getChartData();
+  let chartColor = '#2E607D';
+
+  function getChartData() {
+    const data = [];
+    if (chartKey === 'cases') {
       countyNames.map((item, index) => (data.push({ county: item, cases: countyCases[index] })));
-      chartColor ='#2E607D';
-    } else if(chartKey==='deaths') {
+      chartColor = '#2E607D';
+    } else if (chartKey === 'deaths') {
       countyNames.map((item, index) => (data.push({ county: item, deaths: countyDeaths[index] })));
-      chartColor ='#D61A3C';
+      chartColor = '#D61A3C';
     }
     return data;
   }
 
   function getStats() {
     React.useEffect(() => {
-      if (s !== '') {
-        Socket.emit('search loc', { loc: s });
-      }
       Socket.on('stats', (data) => {
         setState(data.state);
         setCases(data.cases);
@@ -46,7 +41,6 @@ export function Stats() {
         setDeaths(data.deaths);
         setNewDeaths(data.new_deaths);
         setRecovered(data.recovered);
-        setState(data.state);
         setCountyNames(data.countyNames);
         setCountyCases(data.countyCases);
         setCountyDeaths(data.countyDeaths);
@@ -55,41 +49,47 @@ export function Stats() {
         Socket.off('stats');
         Socket.off('search loc');
       };
-    }, []);
+    });
   }
-  
+
   function handleChange(e) {
     setChartKey(e.target.value);
-    document.getElementById('chart-menu').value=e.target.value;
-    chartData=getChartData();
-  } 
+    document.getElementById('chart-menu').value = e.target.value;
+    chartData = getChartData();
+  }
 
   getStats();
-  chartData=getChartData();
-  
+  chartData = getChartData();
+
   return (
     <div className="stats-content">
       <h1 className="stats-h1">COVID-19 Statistics</h1>
-      <hr id='hr-5'/>
-      <div className='stats-menu-div'>
-        <Menu className='stats-state-menu'/>
+      <hr id="hr-5" />
+      <div className="stats-menu-div">
+        <Menu className="stats-state-menu" />
       </div>
-      
+
       <h2 className="stats-h2">State Statistics</h2>
-      <hr id='hr-6'/>
+      <hr id="hr-6" />
       <div className="state-stats">
         <div className="state-stat-div">
           <p className="stat-label">Cases</p>
           <div className="stat-circle">
             <p className="stat-total">{cases.toLocaleString()}</p>
-            <p className="stat-new">+{newCases.toLocaleString()}</p>
+            <p className="stat-new">
+              +
+              {newCases.toLocaleString()}
+            </p>
           </div>
         </div>
         <div className="state-stat-div">
           <p className="stat-label">Deaths</p>
           <div className="stat-circle">
             <p className="stat-total">{deaths.toLocaleString()}</p>
-            <p className="stat-new">+{newDeaths.toLocaleString()}</p>
+            <p className="stat-new">
+              +
+              {newDeaths.toLocaleString()}
+            </p>
           </div>
         </div>
         <div className="state-stat-div">
@@ -100,9 +100,9 @@ export function Stats() {
           </div>
         </div>
       </div>
-      
+
       <h2 className="stats-h2">County Statistics</h2>
-      <hr id='hr-6'/>
+      <hr id="hr-6" />
       <form className="chart-menu">
         <label htmlFor="chart-menu">Statistic: </label>
         <select id="chart-menu" name="chart-menu" value={chartKey} onChange={handleChange}>
@@ -110,7 +110,7 @@ export function Stats() {
           <option value="deaths">deaths</option>
         </select>
       </form>
-      
+
       <div id="chart">
         <BarChart
           width={1000}
@@ -122,7 +122,7 @@ export function Stats() {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis type="number" orientation='bottom'/>
+          <XAxis type="number" orientation="bottom" />
           <YAxis dataKey="county" type="category" />
           <Tooltip />
           <Legend verticalAlign="top" wrapperStyle={{ lineHeight: '40px' }} />
