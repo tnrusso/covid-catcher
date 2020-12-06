8f4960beaa2a2468a82014961d081495072f36dc
Tim Russo <ec2-user@ip-172-31-73-67.ec2.internal>
2020-12-06 22:59:13 +0000

    Lint faq page file

--- scripts/Faq.jsx
+++ scripts/Faq.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars, no-shadow, react/no-array-index-key, no-param-reassign */
 import * as React from 'react';
 import { Socket } from './Socket';
 
@@ -8,44 +9,44 @@ export function Faq() {
     Socket.emit('faq categories');
     return [];
   });
-  
+
   React.useEffect(() => {
     Socket.on('faq category list', (data) => {
       setCategories(data);
     });
     return () => Socket.off('faq category list');
   });
-  
+
   function getQuestions(c) {
-    if(category == c) {
-      c='';
+    if (category === c) {
+      c = '';
     }
     setCategory(c);
     Socket.emit('faq questions', c);
   }
-  
+
   React.useEffect(() => {
     Socket.on('faq list', (data) => {
-        setQuestions(data);
+      setQuestions(data);
     });
     return () => Socket.off('faq list');
   });
-  
 
   return (
     <div className="faq-div">
       <h1 className="faq-h1">Frequently Asked Questions</h1>
       <hr />
-      
+
       <div className="faq-frame">
         <ul className="faq-category-list">
-        
-          {(categories||[]).map((cat, index) => (
+
+          {(categories || []).map((cat, index) => (
             <li key={index} id={cat}>
-              <button className="faq-category-button" onClick={(e)=>getQuestions(cat)}><b>{cat}</b></button>
-              {cat == category &&
+              <button className="faq-category-button" onClick={(e) => getQuestions(cat)}><b>{cat}</b></button>
+              {cat === category
+                && (
                 <ul className="faq-ul">
-          
+
                   {questions.map((faq, index) => (
                     <li key={index}>
                       <p className="faq-question">{faq.question}</p>
@@ -53,7 +54,7 @@ export function Faq() {
                     </li>
                   ))}
                 </ul>
-              }
+                )}
             </li>
           ))}
         </ul>
