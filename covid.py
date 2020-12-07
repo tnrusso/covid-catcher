4b1f0c2a670a9da18d4cf4f75b8ad84d346afa7c
Tim Russo <ec2-user@ip-172-31-73-67.ec2.internal>
2020-12-07 00:06:42 +0000

    Lint covid, location api files, models.py

--- covid.py
+++ covid.py
@@ -1,143 +1,198 @@
-#pylint: disable=C0103
+# pylint: disable=C0103, R0903, R0911, R0914, R0913
 """COVID API CALLS STATES AND COUNTY"""
 import requests
 
-def get_covid_stats_by_county(state,county):
+
+def get_covid_stats_by_county(state, county):
     """API Call for county stats"""
-    url = ('https://corona.lmao.ninja/v2/jhucsse/counties/' + county)
-    response=requests.get(url)
+    url = "https://corona.lmao.ninja/v2/jhucsse/counties/" + county
+    response = requests.get(url)
     data = response.json()
     counties = []
     for res in data:
-        if res["province"]==state:
-            county1 = res['county']
-            updatedAt = res['updatedAt']
-            stats = res['stats']
-            confirmed = stats['confirmed']
-            deaths = stats['deaths']
-            recovered = stats['recovered']
-            counties.append(CountyStats(state,county1,updatedAt,confirmed,deaths,recovered))
-            #return CountyStats(state,county,updatedAt,confirmed,deaths,recovered)
+        if res["province"] == state:
+            county1 = res["county"]
+            updatedAt = res["updatedAt"]
+            stats = res["stats"]
+            confirmed = stats["confirmed"]
+            deaths = stats["deaths"]
+            recovered = stats["recovered"]
+            counties.append(
+                CountyStats(state, county1, updatedAt, confirmed, deaths, recovered)
+            )
+            # return CountyStats(state,county,updatedAt,confirmed,deaths,recovered)
     return counties
+
+
 def get_covid_stats_by_state(state):
     """API Call for state stats"""
-    url = ('https://corona.lmao.ninja/v2/states/'+ state +'?yesterday=')
-    response=requests.get(url)
+    url = "https://corona.lmao.ninja/v2/states/" + state + "?yesterday="
+    response = requests.get(url)
     data = response.json()
-    cases = data['cases']
-    todaysCases = data['todayCases']
-    deaths = data['deaths']
-    todayDeaths = data['todayDeaths']
-    recovered = data['recovered']
-    activeCases = data['active']
-    tests = data['tests']
-    casesPerMil=data['casesPerOneMillion']
-    deathsPerMil=data['deathsPerOneMillion']
-    testsPerMil= data['testsPerOneMillion']
+    cases = data["cases"]
+    todaysCases = data["todayCases"]
+    deaths = data["deaths"]
+    todayDeaths = data["todayDeaths"]
+    recovered = data["recovered"]
+    activeCases = data["active"]
+    tests = data["tests"]
+    casesPerMil = data["casesPerOneMillion"]
+    deathsPerMil = data["deathsPerOneMillion"]
+    testsPerMil = data["testsPerOneMillion"]
 
-    return StateStats(state,cases,todaysCases,activeCases,casesPerMil,deaths,todayDeaths,deathsPerMil,recovered,tests,testsPerMil)
+    return StateStats(
+        state,
+        cases,
+        todaysCases,
+        activeCases,
+        casesPerMil,
+        deaths,
+        todayDeaths,
+        deathsPerMil,
+        recovered,
+        tests,
+        testsPerMil,
+    )
+
 
 def get_covid_stats_for_all_states():
     """API Call for all states stats"""
-    url = ('https://corona.lmao.ninja/v2/states/')
-    response=requests.get(url)
+    url = "https://corona.lmao.ninja/v2/states/"
+    response = requests.get(url)
     data = response.json()
-    
+
     states = []
-    
+
     for state in data:
-        stateName = state['state']
-        
-        cases = state['cases']
-        todaysCases = state['todayCases']
-        deaths = state['deaths']
-        todayDeaths = state['todayDeaths']
-        recovered = state['recovered']
-        activeCases = state['active']
-        tests = state['tests']
-        casesPerMil=state['casesPerOneMillion']
-        deathsPerMil=state['deathsPerOneMillion']
-        testsPerMil= state['testsPerOneMillion']
-        
-        population = state['population']
-        
-        if population!=0:
-            millionsOfPop = population/1000000.0
-            activeCasesPerMil = activeCases/millionsOfPop
+        stateName = state["state"]
+
+        cases = state["cases"]
+        todaysCases = state["todayCases"]
+        deaths = state["deaths"]
+        todayDeaths = state["todayDeaths"]
+        recovered = state["recovered"]
+        activeCases = state["active"]
+        tests = state["tests"]
+        casesPerMil = state["casesPerOneMillion"]
+        deathsPerMil = state["deathsPerOneMillion"]
+        testsPerMil = state["testsPerOneMillion"]
+
+        population = state["population"]
+
+        if population != 0:
+            millionsOfPop = population / 1000000.0
+            activeCasesPerMil = activeCases / millionsOfPop
             color = get_color(activeCasesPerMil)
-            states.append(StateStats(stateName,cases,todaysCases,activeCases,casesPerMil,deaths,todayDeaths,deathsPerMil,recovered,tests,testsPerMil,color))
+            states.append(
+                StateStats(
+                    stateName,
+                    cases,
+                    todaysCases,
+                    activeCases,
+                    casesPerMil,
+                    deaths,
+                    todayDeaths,
+                    deathsPerMil,
+                    recovered,
+                    tests,
+                    testsPerMil,
+                    color,
+                )
+            )
         else:
-            states.append(StateStats(stateName,cases,todaysCases,activeCases,casesPerMil,deaths,todayDeaths,deathsPerMil,recovered,tests,testsPerMil))
-    
+            states.append(
+                StateStats(
+                    stateName,
+                    cases,
+                    todaysCases,
+                    activeCases,
+                    casesPerMil,
+                    deaths,
+                    todayDeaths,
+                    deathsPerMil,
+                    recovered,
+                    tests,
+                    testsPerMil,
+                )
+            )
+
     return states
 
+
 def get_color(activePerMillion):
-    activePer100k = activePerMillion/10.0
+    """Colors for USA map"""
+    activePer100k = activePerMillion / 10.0
     if activePer100k < 100:
-        return '#FFFFFF'
+        return "#FFFFFF"
     elif activePer100k < 500:
-        return '#F1E2E2'
+        return "#F1E2E2"
     elif activePer100k < 1000:
-        return '#E3C6C6'
+        return "#E3C6C6"
     elif activePer100k < 1500:
-        return '#D5AAAA'
+        return "#D5AAAA"
     elif activePer100k < 2000:
-        return '#C78D8D'
+        return "#C78D8D"
     elif activePer100k < 2500:
-        return '#B97171'
+        return "#B97171"
     elif activePer100k < 3000:
-        return '#AB5555'
+        return "#AB5555"
     elif activePer100k < 3500:
-        return '#9D3838'
+        return "#9D3838"
     elif activePer100k < 4000:
-        return '#8F1C1C'
+        return "#8F1C1C"
     else:
-        return '#820000'
-    
+        return "#820000"
+
 
 class CountyStats:
     """Stats for County"""
-    def __init__(self,state,county,updatedAt,confirmed,deaths,recovered):
-        self.state=state
-        self.county=county
-        self.updatedAt=updatedAt
-        self.confirmed=confirmed
-        self.deaths=deaths
-        self.recovered=recovered
+
+    def __init__(self, state, county, updatedAt, confirmed, deaths, recovered):
+        self.state = state
+        self.county = county
+        self.updatedAt = updatedAt
+        self.confirmed = confirmed
+        self.deaths = deaths
+        self.recovered = recovered
+
 
 class StateStats:
     """Stats for State"""
-    def __init__(self,state,cases,todaysCases,activeCases,casesPerMillion,deaths,todayDeaths,deathsPerMillion,recovered,tests,testsPerPerMillion,color='#FFFFFF'):
-        self.state=state
-        self.cases=cases
-        self.todaysCases=todaysCases
-        self.activeCases=activeCases
-        self.casesPerMillion=casesPerMillion
-        self.deaths=deaths
-        self.todayDeaths=todayDeaths
-        self.deathsPerMillion=deathsPerMillion
-        self.recovered=recovered
-        self.tests=tests
-        self.testsPerPerMillion=testsPerPerMillion
+
+    def __init__(
+        self, state, cases, todaysCases, activeCases, casesPerMillion, deaths, todayDeaths, deathsPerMillion, recovered, tests, testsPerPerMillion, color="#FFFFFF",
+    ):
+        self.state = state
+        self.cases = cases
+        self.todaysCases = todaysCases
+        self.activeCases = activeCases
+        self.casesPerMillion = casesPerMillion
+        self.deaths = deaths
+        self.todayDeaths = todayDeaths
+        self.deathsPerMillion = deathsPerMillion
+        self.recovered = recovered
+        self.tests = tests
+        self.testsPerPerMillion = testsPerPerMillion
         self.color = color
-'''
-county = get_covid_stats_by_county("New Jersey","Passaic")
-print(county.state)
-print(county.county)
-print(county.updatedAt)
-print(county.confirmed)
-print(county.deaths)
-print(county.recovered)
-state = get_covid_stats_by_state("New Jersey")
-print(state.state)
-print(state.cases)
-print(state.todaysCases)
-print(state.activeCases)
-print(state.casesPerMillion)
-print(state.deaths)
-print(state.todayDeaths)
-print(state.deathsPerMillion)
-print(state.recovered)
-print(state.tests)
-print(state.testsPerPerMillion)
-'''
+
+
+
+# county = get_covid_stats_by_county("New Jersey","Passaic")
+# print(county.state)
+# print(county.county)
+# print(county.updatedAt)
+# print(county.confirmed)
+# print(county.deaths)
+# print(county.recovered)
+# state = get_covid_stats_by_state("New Jersey")
+# print(state.state)
+# print(state.cases)
+# print(state.todaysCases)
+# print(state.activeCases)
+# print(state.casesPerMillion)
+# print(state.deaths)
+# print(state.todayDeaths)
+# print(state.deathsPerMillion)
+# print(state.recovered)
+# print(state.tests)
+# print(state.testsPerPerMillion)
--- location.py
+++ location.py
@@ -1,38 +1,55 @@
+# pylint: disable=C0103, R0903, R0911, R0914, R0913, R0902
 """API Call to get User's Location"""
-import requests
 import os
 from os.path import join, dirname
+import requests
 from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), "api-keys.env")
 load_dotenv(dotenv_path)
 
+
 def get_location(ip):
     """API Call for Location"""
-    url = ('http://api.ipstack.com/'
-        ''+ip+'?'
-        'access_key='+ os.environ['LOCATION_API_KEY']+''
-        '&format=1')
-    response=requests.get(url)
+    url = (
+        "http://api.ipstack.com/"
+        "" + ip + "?"
+        "access_key=" + os.environ["LOCATION_API_KEY"] + ""
+        "&format=1"
+    )
+    response = requests.get(url)
     data = response.json()
-    country_code = data['country_code']
-    country_name = data['country_name']
-    state_code = data['region_code']
-    state = data['region_name']
-    city = data['city']
-    zipcode = data['zip']
-    latitude = data['latitude']
-    longitude = data['longitude']
-    return Location(country_code,country_name,state_code,state,city,zipcode,latitude,longitude)
+    country_code = data["country_code"]
+    country_name = data["country_name"]
+    state_code = data["region_code"]
+    state = data["region_name"]
+    city = data["city"]
+    zipcode = data["zip"]
+    latitude = data["latitude"]
+    longitude = data["longitude"]
+    return Location(
+        country_code,
+        country_name,
+        state_code,
+        state,
+        city,
+        zipcode,
+        latitude,
+        longitude,
+    )
+
+
 class Location:
     """Location Class"""
-    def __init__(self, country_code,country_name,state_code,state,city,zipcode,latitude,longitude):
-        self.country_code=country_code
-        self.country_name=country_name
-        self.state_code=state_code
-        self.state=state
-        self.city=city
-        self.zipcode=zipcode
-        self.latitude=latitude
-        self.longitude=longitude
-        
+
+    def __init__(
+        self, country_code, country_name, state_code, state, city, zipcode, latitude, longitude,
+    ):
+        self.country_code = country_code
+        self.country_name = country_name
+        self.state_code = state_code
+        self.state = state
+        self.city = city
+        self.zipcode = zipcode
+        self.latitude = latitude
+        self.longitude = longitude
