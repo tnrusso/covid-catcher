03574547fc99ee92bd9192cf599b0641dd97da87
carlososgueda <ec2-user@ip-172-31-51-40.ec2.internal>
2020-12-09 00:20:11 +0000

    eslint map files

--- scripts/InfoMap.jsx
+++ scripts/InfoMap.jsx
@@ -1,10 +1,8 @@
+/* eslint-disable no-unused-vars,import/no-extraneous-dependencies */
 import * as React from 'react';
-import H from '@here/maps-api-for-javascript';
 import ReactDOM from 'react-dom';
 import { Socket } from './Socket';
-import {Map} from './Map';
-import { Articles } from './Articles';
-import { Content } from './Content';
+import { Map } from './Map';
 
 export function InfoMap() {
   const [title, setTitle] = React.useState([]);
@@ -32,14 +30,13 @@ export function InfoMap() {
         setLatu(data.user_lat);
         setLngu(data.user_lng);
         setKey(data.key);
-        if(firstRun!=true) {renderMap(data);}
-        else {firstRun=false;}
+        if (firstRun != true) { renderMap(data); } else { firstRun = false; }
       });
-    
-        return () =>  Socket.off('site page');
-    },[]);
+
+      return () => Socket.off('site page');
+    }, []);
   }
-  
+
   function renderMap(data) {
     ReactDOM.unmountComponentAtNode(document.getElementById('mapping'));
     ReactDOM.render(<Map
@@ -59,70 +56,64 @@ export function InfoMap() {
       web3={data.web[ind + 2]}
       userLat={data.user_lat}
       userLng={data.user_lng}
-  />,document.getElementById('mapping'));
+    />, document.getElementById('mapping'));
   }
-  
+
   function handleSubmit(event) {
-    
     const area = document.getElementById('newArea');
 
     Socket.emit('search location', {
       area: area.value,
     });
-
-    console.log(`Sent the address ${area.value}`);
-
     area.value = '';
     event.preventDefault();
   }
 
-
-  
-    getInfoMap();
+  getInfoMap();
 
   if (key != '') {
-   var m = (
-   <div className="testing-content">
+    var m = (
+      <div className="testing-content">
         <div className="testing-map">
-        <div id="mapping">
-        <Map
-            align="right"
-            key1={key}
-            titleplace1={title[ind]}
-            lat1={latitudes[ind]}
-            lng1={longitudes[ind]}
-            web1={web[ind]}
-            titleplace2={title[ind + 1]}
-            lat2={latitudes[ind + 1]}
-            lng2={longitudes[ind + 1]}
-            web2={web[ind + 1]}
-            titleplace3={title[ind + 2]}
-            lat3={latitudes[ind + 2]}
-            lng3={longitudes[ind + 2]}
-            web3={web[ind + 2]}
-            userLat={ulatitude}
-            userLng={ulongitude}
-          />
+          <div id="mapping">
+            <Map
+              align="right"
+              key1={key}
+              titleplace1={title[ind]}
+              lat1={latitudes[ind]}
+              lng1={longitudes[ind]}
+              web1={web[ind]}
+              titleplace2={title[ind + 1]}
+              lat2={latitudes[ind + 1]}
+              lng2={longitudes[ind + 1]}
+              web2={web[ind + 1]}
+              titleplace3={title[ind + 2]}
+              lat3={latitudes[ind + 2]}
+              lng3={longitudes[ind + 2]}
+              web3={web[ind + 2]}
+              userLat={ulatitude}
+              userLng={ulongitude}
+            />
           </div>
         </div>
 
         <ul className="testing-ul">
           {title.map((siteTitle, index) => (
-          
+
             <li className="testing-li" key={index}>
-            
+
               <p className="testing-title">{siteTitle}</p>
               <p className="testing-desc">{address[index]}</p>
               <p className="testing-source">
                 Tel:
-              <i>{phone[index]}</i>
+                <i>{phone[index]}</i>
               </p>
               <p className="testing-desc">
                 Distance :
-              <i>{miles[index]}</i>
+                <i>{miles[index]}</i>
                 {' '}
                 miles
-            </p>
+              </p>
               <a className="testing-link" href={web[index]}>Go To Website</a>
               <hr id="hr-9" />
             </li>
@@ -130,19 +121,20 @@ export function InfoMap() {
           ))}
 
         </ul>
-      </div>)
+      </div>
+    );
   }
 
   return (
-    
+
     <div className="testing-wrapper">
       <h1 className="testing-h1">Nearest COVID-19 Testing Locations</h1>
       <hr id="hr-8" />
       <form onSubmit={handleSubmit} className="navbar-form d-flex justify-content-center w-100 navbar-right">
         <input id="newArea" placeholder="Enter Address or City" className="city-input" />
-        <button className="city-submit" >Search</button>
+        <button className="city-submit">Search</button>
       </form>
-      
+
       {m}
     </div>
   );
--- scripts/Map.jsx
+++ scripts/Map.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars, react/prop-types,import/no-extraneous-dependencies */
 import * as React from 'react';
 import H from '@here/maps-api-for-javascript';
 import * as ReactDOM from 'react-dom';
@@ -7,10 +8,9 @@ export const Map = (props) => {
   const mapRef = React.useRef(null);
   React.useLayoutEffect(() => {
     if (!mapRef.current) return;
-    console.log(ReactDOM.findDOMNode(mapRef.current))
-    const H = window.H;
+    const { H } = window;
     const platform = new H.service.Platform({
-        apikey: props.key1
+      apikey: props.key1,
     });
     const defaultLayers = platform.createDefaultLayers();
     const hMap = new H.Map(mapRef.current, defaultLayers.vector.normal.map, {
@@ -22,53 +22,52 @@ export const Map = (props) => {
 
     const ui = H.ui.UI.createDefault(hMap, defaultLayers);
     const home = new H.map.Marker({ lat: props.userLat, lng: props.userLng });
-      home.addEventListener('tap', (event) => {
-        const  bubble = new H.ui.InfoBubble(
-          { lat: props.userLat, lng: props.userLng },
-          {
-            content: '<b> You are Here! </b>',
-          },
-        );
-        ui.addBubble(bubble);
-      }, false);
-       const  endMarker1 = new H.map.Marker({ lat: props.lat1, lng: props.lng1 });
-      endMarker1.addEventListener('tap', (event) => {
-        const bubble = new H.ui.InfoBubble(
-          { lat: props.lat1, lng: props.lng1 },
-          {
-            content: `<a href=${props.web1}>${props.titleplace1}</a>`,
-          },
-        );
-        ui.addBubble(bubble);
-      }, false);
-      const endMarker2 = new H.map.Marker({ lat: props.lat2, lng: props.lng2 });
-      endMarker2.addEventListener('tap', (event) => {
-        const bubble = new H.ui.InfoBubble(
-          { lat: props.lat2, lng: props.lng2 },
-          {
-            content: `<a href=${props.web2}>${props.titleplace2}</a>`,
-          },
-        );
-        ui.addBubble(bubble);
-      }, false);
-      const  endMarker3 = new H.map.Marker({ lat: props.lat3, lng: props.lng3 });
-      endMarker3.addEventListener('tap', (event) => {
-        const bubble= new H.ui.InfoBubble(
-          { lat: props.lat3, lng: props.lng3 },
-          {
-            content: `<a href=${props.web3}>${props.titleplace3}</a>`,
-          },
-        );
-        ui.addBubble(bubble);
-      }, false);
-      hMap.addObjects([home,endMarker1, endMarker2, endMarker3]);
+    home.addEventListener('tap', (event) => {
+      const bubble = new H.ui.InfoBubble(
+        { lat: props.userLat, lng: props.userLng },
+        {
+          content: '<b> You are Here! </b>',
+        },
+      );
+      ui.addBubble(bubble);
+    }, false);
+    const endMarker1 = new H.map.Marker({ lat: props.lat1, lng: props.lng1 });
+    endMarker1.addEventListener('tap', (event) => {
+      const bubble = new H.ui.InfoBubble(
+        { lat: props.lat1, lng: props.lng1 },
+        {
+          content: `<a href=${props.web1}>${props.titleplace1}</a>`,
+        },
+      );
+      ui.addBubble(bubble);
+    }, false);
+    const endMarker2 = new H.map.Marker({ lat: props.lat2, lng: props.lng2 });
+    endMarker2.addEventListener('tap', (event) => {
+      const bubble = new H.ui.InfoBubble(
+        { lat: props.lat2, lng: props.lng2 },
+        {
+          content: `<a href=${props.web2}>${props.titleplace2}</a>`,
+        },
+      );
+      ui.addBubble(bubble);
+    }, false);
+    const endMarker3 = new H.map.Marker({ lat: props.lat3, lng: props.lng3 });
+    endMarker3.addEventListener('tap', (event) => {
+      const bubble = new H.ui.InfoBubble(
+        { lat: props.lat3, lng: props.lng3 },
+        {
+          content: `<a href=${props.web3}>${props.titleplace3}</a>`,
+        },
+      );
+      ui.addBubble(bubble);
+    }, false);
+    hMap.addObjects([home, endMarker1, endMarker2, endMarker3]);
     // This will act as a cleanup to run once this hook runs again.
     // This includes when the component un-mounts
     return () => {
       hMap.dispose();
-      console.log('unmounting...');
     };
   }, [mapRef]); // This will run this hook every time this ref is updated
-  
-  return <div id="real" ref={mapRef}  />;
+
+  return <div id="real" ref={mapRef} />;
 };
